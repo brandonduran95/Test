2ab430af1d6f331a62c269458a743920e8018da3
Brando Duran <brandon_duran@comcast.com>
2024-03-25 13:36:45 -0400

    First commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Duran, Brandon (Contractor) on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
